f6472da7a700f630100bfb22bbcee676a6e359fb
asg0416 <73174694+asg0416@users.noreply.github.com>
2021-07-29 15:47:33 +0900

    edit: focus css conflict edit

--- src/components/UploadInput.js
+++ src/components/UploadInput.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Grid, Text, Input } from '../elements';
-import { useState } from 'react';
-import theme from '../styles/theme';
-import logger from '../shared/Console';
+import React from "react";
+import styled from "styled-components";
+import { Grid, Text, Input } from "../elements";
+import { useState } from "react";
+import theme from "../styles/theme";
+import logger from "../shared/Console";
 
 const UploadInput = (props) => {
   const { color, fontSize } = theme;
 
   React.useEffect(() => {
-    logger('uploadinput 페이지', props);
+    logger("uploadinput 페이지", props);
   }, []);
 
   const [post_info, setPostInfo] = useState(
@@ -22,11 +22,11 @@ const UploadInput = (props) => {
           foodCategory: props.post_info.foodCategory,
         }
       : {
-          place: '',
-          restaurant: '',
-          headCount: '',
-          appointmentTime: '',
-          foodCategory: '',
+          place: "",
+          restaurant: "",
+          headCount: "",
+          appointmentTime: "",
+          foodCategory: "",
         }
   );
 
@@ -83,57 +83,7 @@ const UploadInput = (props) => {
             </FocusWithin>
           </Grid>
 
-//           <FocusWithin>
-
-//           <Text
-//             padding="2.4rem 0 0.8rem"
-//             color="#888E95"
-//             bold="700"
-//             size={fontSize.base}
-//           >
-//             모집 인원 수
-//           </Text>
-//           {/* <Input
-//             type="number"
-//             min="0"
-//             border="1px solid #C7C8CE"
-//             padding="1.5rem 1.3rem"
-//             size={fontSize.base}
-//             color={color.bg60}
-//             placeholder="모집할 인원 수를 입력해주세요."
-//             value={`${post_info.headCount}`}
-//             _onChange={(e) => {
-//               setPostInfo({ ...post_info, headCount: e.target.value });
-//               props.onChange({ headCount: e.target.value });
-//             }}
-//           ></Input> */}
-//           <Grid
-//             border="1px solid #C7C8CE"
-//             padding="0 1.3rem"
-//             radius="1.2rem"
-//             height="auto"
-//           >
-//             <Select
-//               value={`${post_info.headCount}`}
-//               onChange={(e) => {
-//                 setPostInfo({
-//                   ...post_info,
-//                   headCount: e.target.value,
-//                 });
-//                 props.onChange({ headCount: e.target.value });
-//               }}
-//             >
-//               <option value="none" hidden defaultValue>
-//                 모집인원을 선택해주세요.
-//               </option>
-//               <option value="2">2명</option>
-//               <option value="3">3명</option>
-//               <option value="4">4명</option>
-//             </Select>
-//           </Grid>
-//             <Text color="red" size={fontSize.tiny} padding="0.5rem 1rem 0">5인 이상 집합금지로 인원에 제한이 있습니다.</Text>
-//           <Grid>
-
+          <FocusWithin>
             <Text
               padding="2.4rem 0 0.8rem"
               color="#888E95"
@@ -142,20 +92,34 @@ const UploadInput = (props) => {
             >
               모집 인원 수
             </Text>
-            <Input
-              type="number"
-              min="0"
+            <Grid
               border="1px solid #C7C8CE"
-              padding="1.5rem 1.3rem"
-              size={fontSize.base}
-              color={color.bg60}
-              placeholder="모집할 인원 수를 입력해주세요."
-              value={`${post_info.headCount}`}
-              _onChange={(e) => {
-                setPostInfo({ ...post_info, headCount: e.target.value });
-                props.onChange({ headCount: e.target.value });
-              }}
-            ></Input>
+              padding="0 1.3rem"
+              radius="1.2rem"
+              height="auto"
+            >
+              <Select
+                value={`${post_info.headCount}`}
+                onChange={(e) => {
+                  setPostInfo({
+                    ...post_info,
+                    headCount: e.target.value,
+                  });
+                  props.onChange({ headCount: e.target.value });
+                }}
+              >
+                <option value="none" hidden defaultValue>
+                  모집인원을 선택해주세요.
+                </option>
+                <option value="2">2명</option>
+                <option value="3">3명</option>
+                <option value="4">4명</option>
+              </Select>
+            </Grid>
+            <Text color="red" size={fontSize.tiny} padding="0.5rem 1rem 0">
+              5인 이상 집합금지로 인원에 제한이 있습니다.
+            </Text>
+            <Grid></Grid>
           </FocusWithin>
           <Grid>
             <FocusWithin>
@@ -184,43 +148,45 @@ const UploadInput = (props) => {
               ></Input>
             </FocusWithin>
           </Grid>
-          <Grid margin="0 auto 1rem">
-            <Text
-              padding="2.4rem 0 0.8rem"
-              color="#888E95"
-              bold="700"
-              size={fontSize.base}
-            >
-              음식 카테고리
-            </Text>
-            <Grid
-              border="1px solid #C7C8CE"
-              padding="0 1.3rem"
-              radius="1.2rem"
-              height="auto"
-            >
-              <Select
-                value={post_info.foodCategory}
-                onChange={(e) => {
-                  setPostInfo({
-                    ...post_info,
-                    foodCategory: e.target.value,
-                  });
-                  props.onChange({ foodCategory: e.target.value });
-                }}
+          <FocusWithin>
+            <Grid margin="0 auto 1rem">
+              <Text
+                padding="2.4rem 0 0.8rem"
+                color="#888E95"
+                bold="700"
+                size={fontSize.base}
               >
-                <option value="none" defaultValue hidden>
-                  음식 카테고리를 선택해주세요.
-                </option>
-                <option value="한식">한식</option>
-                <option value="중식">중식</option>
-                <option value="일식">일식</option>
-                <option value="양식">양식</option>
-                <option value="카페">카페</option>
-                <option value="기타">기타</option>
-              </Select>
+                음식 카테고리
+              </Text>
+              <Grid
+                border="1px solid #C7C8CE"
+                padding="0 1.3rem"
+                radius="1.2rem"
+                height="auto"
+              >
+                <Select
+                  value={post_info.foodCategory}
+                  onChange={(e) => {
+                    setPostInfo({
+                      ...post_info,
+                      foodCategory: e.target.value,
+                    });
+                    props.onChange({ foodCategory: e.target.value });
+                  }}
+                >
+                  <option value="none" defaultValue hidden>
+                    음식 카테고리를 선택해주세요.
+                  </option>
+                  <option value="한식">한식</option>
+                  <option value="중식">중식</option>
+                  <option value="일식">일식</option>
+                  <option value="양식">양식</option>
+                  <option value="카페">카페</option>
+                  <option value="기타">기타</option>
+                </Select>
+              </Grid>
             </Grid>
-          </Grid>
+          </FocusWithin>
         </Container>
       </Grid>
     </React.Fragment>
